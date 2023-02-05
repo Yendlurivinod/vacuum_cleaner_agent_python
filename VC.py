bbff6f6ae9b34e943251d17c3150c0cf318db227
adr485 <66263972+adr485@users.noreply.github.com>
2023-02-05 19:27:44 +0530

    Update VC.py

--- VC.py
+++ VC.py
@@ -12,7 +12,7 @@ def right():
     vc[1]=vc[1]+1
 def left():
     vc[1]=vc[1]-1
-def BlueBG(s): print("\033[46m{}\033[00m".format(s),end=' ')
+def BlueBG(s): print("\033[46m{}\033[00m".format(s),end=' ')   # Blue background to indicate agent position
 def pr():
     for i in range(n):
         for j in range(n):
@@ -27,15 +27,16 @@ print('enter size')
 n=int(input()) #nxn matrix
 print('enter vacuum cleaner position')
 vc=[0]*2
-vc[0]=int(input())
-vc[1]=int(input())
+vc[0]=int(input())  # x-corordinate
+vc[1]=int(input())  # y-corordinate
 
-print('vacuum cleaner position: ',vc)
-l=[[int(input()) for j in range (n)] for i in range(n)]
+print('vacuum cleaner position is: ',vc)
+print('enter room status 1-dirty 0-clean for all rooms')
+l=[[int(input()) for j in range (n)] for i in range(n)]   #room status 1-dirty, 0-clean
 print('dirt is in following rooms: ')
 pr()
 c=0
-if(vc[0]!=0 and vc[1]!=0):
+if(vc[0]!=0 and vc[1]!=0): # case-2 agent not at (0,0)
     x=vc[0]
     y=vc[1]
     for i in range(y+1):
@@ -55,7 +56,7 @@ if(vc[0]!=0 and vc[1]!=0):
         pr()
         up()
         
-while c<=(n*n-1):
+while c<=(n*n-1): # agent position at (0,0)
     for k in  range(n):
         if isdirty(vc[0],vc[1])==1:
             suck(vc[0],vc[1])
@@ -64,7 +65,7 @@ while c<=(n*n-1):
         pr()
         right()
         c+=1
-    left()
+    left() 
     down()
     if c>=(n*n-1):
         break
